e9a610a16bb617c3caad3e642c8daf5b68f46ae1
Wolfgang Werner <w.werner@gridx.de>
2024-01-12 18:45:47 +0100

    fix: Correct appliance list update

--- examples/react-websockets/client/src/App.js
+++ examples/react-websockets/client/src/App.js
@@ -24,44 +24,57 @@ function App() {
     const payload = JSON.parse(event.data);
 
     if (systems[payload.gatewayID]) {
-      systems[payload.gatewayID].appliances = [...systems[payload.gatewayID].appliances, payload];
+      const appliances = systems[payload.gatewayID].appliances;
+      appliances[payload.applianceID] = payload;
+      systems.appliances = appliances;
     } else {
       systems[payload.gatewayID] = {
         gatewayID: payload.gatewayID,
         systemName: payload.systemName,
-        appliances: [payload],
+        appliances: {
+          [payload.applianceID]: payload,
+        },
       };
     }
 
     setSystems({ ...systems });
   };
 
-
   return (
     <div className="App">
       <div className="systems">
-        {Object.values(systems).map((system) => (
-          <System system={system} key={system.gatewayID}/>
-        ))}
+        {systems &&
+          Object.values(systems).map(
+            (system) =>
+              system.gatewayID && (
+                <System system={system} key={system.gatewayID} />
+              )
+          )}
       </div>
     </div>
   );
 }
 
 const System = ({ system }) => (
-  <div className="system">
+  <div className="system" id={system.gatewayID}>
     <div className="systemName">{system.systemName}</div>
     <div className="appliances">
-      {system.appliances?.map((appliance) => (
-        <Appliance appliance={appliance} key={appliance.applianceID}/>
-      ))}
+      {system.appliances &&
+        Object.values(system.appliances)?.map((appliance) => (
+          <Appliance appliance={appliance} key={appliance.applianceID} />
+        ))}
     </div>
   </div>
 );
 
 const Appliance = ({ appliance }) => (
-  <div className={`appliance ${appliance.online ? "online" : "offline"}`}>
-    {appliance.manufacturer}<br/>{appliance.model}
+  <div
+    id={appliance.applianceID}
+    className={`appliance ${appliance.online ? "online" : "offline"}`}
+  >
+    {appliance.manufacturer}
+    <br />
+    {appliance.model}
   </div>
 );
 
